1feb379f55c2cb0c9bdbcb51eddf5e3ab4603142
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-01-27 19:42:47 +0545

    add styles for custom button component

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -19,7 +19,7 @@
     <label for="answer-b">Answer B value:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB} />
   </div>
-  <Button>Add Poll</Button>
+  <Button type="secondary" flat={true}>Add Poll</Button>
 </form>
 
 <style>
