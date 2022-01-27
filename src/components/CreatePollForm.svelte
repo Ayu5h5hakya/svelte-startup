ac9ce2004c84886e01070f0c53f5c5b3410edc23
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-01-27 18:22:44 +0545

    add basic  form fields for creating new polls

---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,18 @@
+<script></script>
+
+<form>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" />
+  </div>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" />
+  </div>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" />
+  </div>
+</form>
+
+<style></style>
