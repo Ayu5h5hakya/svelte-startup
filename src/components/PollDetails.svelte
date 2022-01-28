484cf537d7c009dbcc99c63fc2c0ed39e808ccca
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-01-28 18:42:15 +0545

    create poll detail component

---
+++ src/components/PollDetails.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let poll;
+</script>
+
+<div class="poll">
+  <h3>{poll.question}</h3>
+  <div class="answer">
+    <div class="percent percent-a" />
+    <span>{poll.answerA} ({poll.voteA})</span>
+  </div>
+  <div class="answer">
+    <div class="percent percent-b" />
+    <span>{poll.answerB} ({poll.voteB})</span>
+  </div>
+</div>
+
+<style></style>
