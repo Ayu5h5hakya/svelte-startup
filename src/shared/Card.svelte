7fb347abc62aeae6ff7da5f8b9e966dc761ddcdb
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-01-29 17:54:22 +0545

    add custom card component

---
+++ src/shared/Card.svelte
@@ -0,0 +1,12 @@
+<div class="card">
+  <slot />
+</div>
+
+<style>
+  .card {
+    background: #fff;
+    padding: 20;
+    border-radius: 6;
+    box-shadow: 0 2 4 rgba(0, 0, 0, 0.1);
+  }
+</style>
