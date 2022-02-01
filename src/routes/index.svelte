9e954cde8cf2fad0d54018747f9fd1df2974f098
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-02-01 13:05:49 +0545

    add grid styling for pokemon list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,23 @@
 <script>
+	import { onDestroy } from 'svelte/internal';
+
+	import { PokeStore } from '../stores/Pokestore.js';
+	let pokemonList = [];
+
+	const unSub = PokeStore.subscribe((data) => {
+		pokemonList = data;
+	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<div class="container">
+	{#each pokemonList as pokemon}
+		<p>{pokemon.name}</p>
+	{/each}
+</div>
+
+<style>
+	.container {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+</style>
