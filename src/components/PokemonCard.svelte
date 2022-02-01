6704f1ff513ed0030b7826efe9c1f7a7e694cdde
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-02-01 13:33:43 +0545

    add proper styles for each pokemon card

--- src/components/PokemonCard.svelte
+++ src/components/PokemonCard.svelte
@@ -2,7 +2,18 @@
 	export let pokemon;
 </script>
 
-<a href={'/pokemon/${pokemon.id}'}>
-	<img src={pokemon.image} alt="" />
-	<p>{pokemon.name}</p>
-</a>
+<div class="card">
+	<a href={'/pokemon/${pokemon.id}'}>
+		<img src={pokemon.image} alt={pokemon.name} />
+		<p>{pokemon.name}</p>
+	</a>
+</div>
+
+<style>
+	.card {
+		text-align: center;
+		padding: 40px;
+		margin: 10px;
+		background: #e74c3c;
+	}
+</style>
