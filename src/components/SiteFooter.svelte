9eebb1e84f532073dae00460526f4b58cf527899
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-02-02 18:19:45 +0545

    add site footer

---
+++ src/components/SiteFooter.svelte
@@ -0,0 +1,11 @@
+<script></script>
+
+<div class="container">
+	<p>Site Language</p>
+</div>
+
+<style>
+	.container {
+		padding: 0 40px 0 40px;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
 	import SiteHeader from '../components/SiteHeader.svelte';
+	import SiteFooter from 'src/components/SiteFooter.svelte';
 </script>
 
 <SiteHeader />
+<SiteFooter />
