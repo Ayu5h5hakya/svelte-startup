692d2547f3e353ff5bc80023e61b2beb2592269c
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-02-03 18:43:33 +0545

    disable bg transparency layer

--- src/components/SiteHeader.svelte
+++ src/components/SiteHeader.svelte
@@ -8,7 +8,7 @@
 		src="https://d35aaqx5ub95lt.cloudfront.net/images/dca3b978d07a7dfc05adb1d5526e9e21.svg"
 		alt=""
 	/>
-	<Modal>
+	<Modal disableFocusTrap={true} styleBg={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}>
 		<Content />
 	</Modal>
 </header>
@@ -18,15 +18,8 @@
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		padding: 0 20px 0 20px;
 	}
 	img {
 		height: 36px;
-	}
-	.header-lang-selector {
-		font-family: 'Varela Round', sans-serif;
-		font-weight: bold;
-		color: white;
-		text-transform: uppercase;
 	}
 </style>
