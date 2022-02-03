7185c2040ed668b02c081444cd0b47c853e4b553
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-02-03 19:31:52 +0545

    add custom modal

--- src/components/SiteHeader.svelte
+++ src/components/SiteHeader.svelte
@@ -1,6 +1,10 @@
 <script>
-	import Modal from 'svelte-simple-modal';
-	import Content from './Content.svelte';
+	import Modal from '../shared/Modal.svelte';
+	let isModalOpen = false;
+
+	const openModal = () => {
+		isModalOpen = true;
+	};
 </script>
 
 <header>
@@ -8,15 +12,12 @@
 		src="https://d35aaqx5ub95lt.cloudfront.net/images/dca3b978d07a7dfc05adb1d5526e9e21.svg"
 		alt=""
 	/>
-	<Modal
-		disableFocusTrap={true}
-		styleBg={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
-		styleWindow={{ boxShadow: '0 2px 5px 0 rgba(0, 0, 0, 0.15)' }}
-		classWindow="modal-wrapper"
-	>
-		<Content />
-	</Modal>
+	<button on:click={openModal}>Press Me!</button>
 </header>
+
+{#if isModalOpen}
+	<Modal />
+{/if}
 
 <style>
 	header {
@@ -26,9 +27,5 @@
 	}
 	img {
 		height: 36px;
-	}
-	.modal-wrapper {
-		position: absolute;
-		right: 0;
 	}
 </style>
