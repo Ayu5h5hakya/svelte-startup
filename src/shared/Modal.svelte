61f9d4e1fd7bc6ba830ab625837249c5476095b1
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-02-04 18:38:02 +0545

    align modal content to top right

--- src/shared/Modal.svelte
+++ src/shared/Modal.svelte
@@ -1,15 +1,13 @@
 <script></script>
 
 <div class="modal">
-	<div class="backdrop" />
 	<div class="content">Text goes here</div>
 </div>
 
 <style>
-	.backdrop {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		background-color: rgba(0, 0, 0, 0.4);
+	.modal {
+		position: fixed;
+		top: 0;
+		right: 0;
 	}
 </style>
