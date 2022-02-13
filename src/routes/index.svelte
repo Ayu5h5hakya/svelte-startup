f78c5427ecc2fb5faf4457fbdc857256e81bae8e
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-02-13 17:47:07 +0545

    add auth section in home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,3 +3,16 @@
 </script>
 
 <SiteHeader />
+<div class="homepage-auth-section">
+	The free, fun and effective way to learn a language!
+	<button>GET STARTED</button>
+	<button>I ALREADY HAVE AN ACCOUNT</button>
+</div>
+
+<style>
+	.homepage-auth-section {
+		display: flex;
+		flex-direction: column;
+		max-width: 500px;
+	}
+</style>
