e6ae7a21b3dd6107b5c48952644d13ec650e6d00
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-02-15 14:10:18 +0545

    refine UI for homepage welcome prompt

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,20 +3,42 @@
 </script>
 
 <SiteHeader />
-<img
-	src="https://d35aaqx5ub95lt.cloudfront.net/images/331a32943ff73be28537dfc7469d5639.svg"
-	alt="globe"
-/>
 <div class="homepage-auth-section">
-	The free, fun and effective way to learn a language!
-	<button>GET STARTED</button>
-	<button>I ALREADY HAVE AN ACCOUNT</button>
+	<img
+		src="https://d35aaqx5ub95lt.cloudfront.net/images/331a32943ff73be28537dfc7469d5639.svg"
+		alt="globe"
+	/>
+	<h2>The free, fun and effective way to learn a language!</h2>
+	<button class="dimensions get-started">GET STARTED</button>
+	<button class="dimensions account-exists">I ALREADY HAVE AN ACCOUNT</button>
 </div>
 
 <style>
 	.homepage-auth-section {
+		color: white;
 		display: flex;
 		flex-direction: column;
 		max-width: 500px;
+		align-items: center;
+	}
+
+	h2 {
+		text-align: center;
+	}
+
+	.dimensions {
+		min-width: 220px;
+		min-height: 30px;
+		color: white;
+		margin-top: 5px;
+		margin-bottom: 5px;
+	}
+
+	.get-started {
+		background-color: #58cc02;
+	}
+
+	.account-exists {
+		background-color: #1cb0f6;
 	}
 </style>
