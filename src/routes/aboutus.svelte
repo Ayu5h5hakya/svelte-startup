6e6764e67ea6baf17c952662d71960fee597ce5a
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-03-03 17:24:17 +0545

    add seperate missions component

--- src/routes/aboutus.svelte
+++ src/routes/aboutus.svelte
@@ -1,40 +1,11 @@
 <script>
-	import Divider from '../shared/Divider.svelte';
-	import MissionCard from '../components/MissionCard.svelte';
+	import Missions from '../components/Missions.svelte';
 	import AboutUsTabs from '../components/AboutUsTabs.svelte';
 </script>
 
 <div class="about-us">
 	<AboutUsTabs />
-	<MissionCard
-		title="Personalized Education"
-		statement="Everyone learns in different ways. For the first time in history, we can analyze how millions
-	of people learn at once to create the most effective educational system possible and tailor it
-	to each student. Our ultimate goal is to give everyone access to a private tutor experience
-	through technology"
-		imageurl="https://d35aaqx5ub95lt.cloudfront.net/images/about/personalized-education.svg"
-	/>
-	<Divider />
-	<MissionCard
-		title="Making learning fun."
-		statement="It's hard to stay motivated when learning online, so we made Duolingo so fun that people would
-		prefer picking up new skills over playing a game."
-		imageurl="https://d35aaqx5ub95lt.cloudfront.net/images/about/learning-fun.svg"
-	/>
-	<Divider />
-	<MissionCard
-		title="Universally accessible."
-		statement="There are over 1.2 billion people learning a language and the majority are doing so to gain
-		access to better opportunities. Unfortunately, learning a language is expensive and
-		inaccessible to most. We created Duolingo so that everyone could have a chance. Free language
-		education – no hidden fees, no premium content, just free. Duolingo is used by the richest man
-		in the world and many Hollywood stars, and at the same time by public schools students in
-		developing countries. We believe true equality is when spending more can't buy you a better
-		education."
-		imageurl="https://d35aaqx5ub95lt.cloudfront.net/images/about/universally-accessible-1.svg"
-		imageurl2="https://d35aaqx5ub95lt.cloudfront.net/images/about/universally-accessible-2.svg"
-	/>
-	<Divider />
+	<Missions />
 </div>
 
 <style>
