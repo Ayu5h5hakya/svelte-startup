960663d3dfe75f616ba52f9e60b18ae496fef957
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-03-09 19:56:50 +0545

    switch image gif to switch features

--- src/components/HIghlightedFeatures.svelte
+++ src/components/HIghlightedFeatures.svelte
@@ -1,6 +1,14 @@
 <script>
+	const gifList = [
+		'https://d35aaqx5ub95lt.cloudfront.net/images/plusLandingPage/2ec4bb4cf764c7adee599b9e5021a475.gif',
+		'https://d35aaqx5ub95lt.cloudfront.net/images/plusLandingPage/fa07db4885751ed282f25d30da7c58a1.gif',
+		'https://d35aaqx5ub95lt.cloudfront.net/images/plusLandingPage/2ec4bb4cf764c7adee599b9e5021a475.gif',
+		'https://d35aaqx5ub95lt.cloudfront.net/images/plusLandingPage/dd6dc3cab1b937311b9e231c6209917e.gif'
+	];
+
+	let currentIndex = 0;
 	const onProgressClick = (index) => {
-		console.log(index);
+		currentIndex = index;
 	};
 </script>
 
@@ -8,15 +16,12 @@
 <h3>Features designed for the fastest, most fun way to learn a language</h3>
 <div class="feature-demo">
 	<ul>
-		<li on:click={() => onProgressClick(1)}>Progress tracker</li>
-		<li on:click={() => onProgressClick(2)}>Ad-Free</li>
-		<li on:click={() => onProgressClick(3)}>Streak Repair</li>
-		<li on:click={() => onProgressClick(4)}>Unlimited Hearts</li>
+		<li on:click={() => onProgressClick(0)}>Progress tracker</li>
+		<li on:click={() => onProgressClick(1)}>Ad-Free</li>
+		<li on:click={() => onProgressClick(2)}>Streak Repair</li>
+		<li on:click={() => onProgressClick(3)}>Unlimited Hearts</li>
 	</ul>
-	<img
-		src="https://d35aaqx5ub95lt.cloudfront.net/images/plusLandingPage/2ec4bb4cf764c7adee599b9e5021a475.gif"
-		alt=""
-	/>
+	<img src={gifList[currentIndex]} alt="" />
 </div>
 
 <style>
