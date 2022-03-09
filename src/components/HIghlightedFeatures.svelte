e258b51f2723e0114f1946fb93ac8b54df9d811c
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-03-09 19:50:42 +0545

    add interactive for highlighted feature

--- src/components/HIghlightedFeatures.svelte
+++ src/components/HIghlightedFeatures.svelte
@@ -1,11 +1,17 @@
+<script>
+	const onProgressClick = (index) => {
+		console.log(index);
+	};
+</script>
+
 <h1>Fast track your progress with Duolingo Plus</h1>
 <h3>Features designed for the fastest, most fun way to learn a language</h3>
 <div class="feature-demo">
 	<ul>
-		<li>Progress tracker</li>
-		<li>Ad-Free</li>
-		<li>Streak Repair</li>
-		<li>Unlimited Hearts</li>
+		<li on:click={() => onProgressClick(1)}>Progress tracker</li>
+		<li on:click={() => onProgressClick(2)}>Ad-Free</li>
+		<li on:click={() => onProgressClick(3)}>Streak Repair</li>
+		<li on:click={() => onProgressClick(4)}>Unlimited Hearts</li>
 	</ul>
 	<img
 		src="https://d35aaqx5ub95lt.cloudfront.net/images/plusLandingPage/2ec4bb4cf764c7adee599b9e5021a475.gif"
