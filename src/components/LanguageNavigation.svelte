4c3a04370dc1d7314bd335b1d2eb43cf09ee33e6
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-03-14 17:51:51 +0545

    add language selector UI slider in home page

--- src/components/LanguageNavigation.svelte
+++ src/components/LanguageNavigation.svelte
@@ -9,6 +9,20 @@
 				stroke-width="2"
 			/></svg
 		>
+		<ul>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+		</ul>
 		<svg class="right" fill="none" viewBox="0 0 16 16"
 			><path
 				d="M6 2L12 8L6 14"
@@ -23,13 +37,26 @@
 
 <style>
 	.navigation-container {
-		display: flex;
 		height: 74px;
 		width: 1004px;
+		margin: 0 auto;
+	}
+
+	nav {
+		display: flex;
+		flex-direction: row;
 	}
 
 	.left,
 	.right {
 		height: 22px;
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	li {
+		display: inline-block;
 	}
 </style>
