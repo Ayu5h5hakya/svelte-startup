ef4a657ac8eaf0555b64f9a25bd5b69fe3666ca7
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-03-24 17:41:57 +0545

    style tabs for following and for you

---
+++ src/components/SectionSelector.svelte
@@ -0,0 +1,12 @@
+<div class="tabs">
+	<p>Following</p>
+	<p>For you</p>
+</div>
+
+<style>
+	.tabs {
+		display: flex;
+		gap: 2rem;
+		color: white;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script>
 	import FollowingSection from '../components/FollowingSection.svelte';
 	import SiteHeader from '../shared/SiteHeader.svelte';
+	import SectionSelector from '../components/SectionSelector.svelte';
 	let activeTab = 0;
 </script>
 
 <SiteHeader />
+<SectionSelector />
 <FollowingSection />
