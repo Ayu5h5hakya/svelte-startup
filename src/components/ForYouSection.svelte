f6c782bf01c836b9c5e6175764935f2af1e71d5a
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2022-03-24 17:49:07 +0545

    add basic UI for For You section

---
+++ src/components/ForYouSection.svelte
@@ -0,0 +1,16 @@
+<div class="welcome">
+	<p>Welcome to the new feed!</p>
+	<p>
+		We’re updating the cards and ranking all the time, so check back regularly. At first, you might
+		need to follow some people or star some repositories to get started
+	</p>
+	<button>Send Feedback</button>
+</div>
+
+<style>
+	.welcome {
+		color: white;
+		border-style: solid;
+		border-color: white;
+	}
+</style>
